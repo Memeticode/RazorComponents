7b00f6280fcad94079cc166634b7b5b9d5f450a7
Memeticode <thatcher.eills@outlook.com>
2023-05-05 23:56:40 -0400

    at

---
+++ RazorComponents/RazorComponentWorkshop/wwwroot/plotlyInterop.js
@@ -0,0 +1,5 @@
+﻿function renderPlotlyChart(elementId, dataJson, layoutJson) {
+    var data = JSON.parse(dataJson);
+    var layout = JSON.parse(layoutJson);
+    Plotly.newPlot(elementId, data, layout);
+}
